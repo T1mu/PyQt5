8d8f08bd4c97e9a0ef934dc530e38b1dc9e15433
t1mu <timj3ly@gmail.com>
2019-07-17 16:55:35 +0800

    fix

--- 007.py
+++ 007.py
@@ -1,39 +1,39 @@
-    #!/usr/bin/python3
-    # -*- coding: utf-8 -*-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+This program creates a statusbar.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: January 2015
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
     
-    """
-    ZetCode PyQt5 tutorial
-    
-    This program creates a statusbar.
-    
-    author: Jan Bodnar
-    website: zetcode.com
-    last edited: January 2015
-    """
-    
-    import sys
-    from PyQt5.QtWidgets import QMainWindow, QApplication
-    
-    
-    class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
         
-        def __init__(self):
-            super().__init__()
-            
-            self.initUI()
-            
-            
-        def initUI(self):              
-            
-            self.statusBar().showMessage('Ready')
-            
-            self.setGeometry(300, 300, 250, 150)
-            self.setWindowTitle('Statusbar')   
-            self.show()
-    
-    
-    if __name__ == '__main__':
+        self.initUI()
         
-        app = QApplication(sys.argv)
-        ex = Example()
-        sys.exit(app.exec_())
+        
+    def initUI(self):              
+        
+        self.statusBar().showMessage('Ready')
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')   
+        self.show()
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
